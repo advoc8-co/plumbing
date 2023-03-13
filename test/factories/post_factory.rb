8e5d6a18af63b8e3e38bf61d96d2537c8d120384
Howard <howardjfraser@gmail.com>
2023-03-13 13:16:00 +1100

    Simplify set up.

--- test/factories/post_factory.rb
+++ test/factories/post_factory.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :post do
     user
-    # company comes from Current.user
+    company
     title { Faker::Lorem.words(number: 4) }
     body { Faker::Lorem.paragraphs(number: 3) }
+    project
   end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class PostTest < ActiveSupport::TestCase
   def setup
     super # initialize Current otherwise next line will fail
-    @post = create(:post, user: Current.user, project: create(:project))
+    @post = create(:post)
   end
 
   test "valid" do
