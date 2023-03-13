48ddd46deab0c516acac0123053400d7af8e25d5
Howard <howardjfraser@gmail.com>
2023-03-14 10:34:18 +1100

    Seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,41 @@
 # Resets
 
-# NOTE: this is more complicated than it might seem. To delete untenanted classes with associations
-# to tenanted classes you, need to set Current.company.
-
-Stakeholder.destroy_all
-Tenant.all { |c| Project.destroy_all }
-Tenant.all { |c| CompanyUser.destroy_all }
-Tenant.all { |c| User.destroy_all }
-Tenant.all { |c| c.destroy! }
+# NOTE: resetting data is more complicated than it might seem. Drop and recreate db for now.
 
 # Creates
 
-abc = Company.create! name: "ABC Co"
-xyz = Company.create! name: "XYZ Co"
-
 multi = User.create! name: Faker::Name.first_name
+
+abc = Company.create! name: "ABC Co"
+abc.users << multi
+xyz = Company.create! name: "XYZ Co"
+xyz.users << multi
 
 4.times { Stakeholder.create! name: Faker::Name.first_name }
 
-Tenant.switch(abc) do
-  2.times { abc.users.create! name: Faker::Name.first_name }
-  abc.users << multi
+Tenant.switch!(abc, multi)
 
-  3.times do
-    Project.create!({name: Faker::Company.bs, stakeholders: Stakeholder.all.sample(2)})
-  end
+2.times { abc.users.create! name: Faker::Name.first_name }
 
-  4.times do
-    Post.create({
-      user: User.all.sample,
-      project: Project.all.sample,
-      title: Faker::Lorem.words(number: 2).join(" "),
-      body: Faker::Lorem.paragraphs(number: 3)
-    })
-  end
+3.times do
+  Project.create!({name: Faker::Company.bs, stakeholders: Stakeholder.all.sample(2)})
 end
 
-Tenant.switch(xyz) do
-  2.times { abc.users.create! name: Faker::Name.first_name }
-  xyz.users << multi
-
-  xyz.projects.create!({
-    name: Faker::Company.bs,
-    stakeholders: Stakeholder.all.sample(3)
+4.times do
+  Post.create({
+    user: User.all.sample,
+    project: Project.all.sample,
+    title: Faker::Lorem.words(number: 2).join(" "),
+    body: Faker::Lorem.paragraphs(number: 3)
   })
 end
+
+Tenant.switch!(xyz, multi)
+
+2.times { abc.users.create! name: Faker::Name.first_name }
+xyz.users << multi
+
+xyz.projects.create!({
+  name: Faker::Company.bs,
+  stakeholders: Stakeholder.all.sample(3)
+})
