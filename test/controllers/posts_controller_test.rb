ff62f86167c54b004769ff361ce980e6742d8256
Howard <howardjfraser@gmail.com>
2023-03-14 17:28:45 +1100

    Improve test / fix bug.

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    # TODO: this call to super is awkward - delegate to a custom setup
+    # method from test_helper?
+    super
+    create_list(:post, 2)
+
+    Tenant.switch!(@second_co, @second_user)
+    create(:post)
+  end
+
   test "index" do
     get posts_url
     assert_select "h1", "Posts"
-    assert_select "#count", "0 posts found"
+    assert_select "#count", "2 posts found"
   end
 end
